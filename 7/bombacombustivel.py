8da1cdf123ac016253e5e36beae3b4bebcb5b01a
Aline de Oliveira Freitas <dreamofaline@gmail.com>
2011-05-29 11:57:49 -0300

    Sixth and Seventh problem from the list of exercises solved

---
+++ 7/bombacombustivel.py
@@ -0,0 +1,29 @@
+#coding: utf-8
+# 7) Crie uma classe que modele uma bomba de combustível e permita definir a capacidade da bomba, o preço do combustível por litro e permita encher a bomba (sempre encher completamente) e consultar sua quantidade de combustível. É necessário também que seja permitido abastecer por valor (retornando a quantidade de combustível abastecida), abastecer por quantidade de litros (retornando o valor a ser pago). Deve ser permitido também alterar o valor do preço por litro. Deve ser tratada a situação de não haver combustível suficiente na bomba para um determinado abastecimento.
+
+class Bomba(object):
+
+    def __init__(self, capacidade_maxima, preco_litro):
+        self.capacidade_maxima = capacidade_maxima
+        self.preco_litro = preco_litro
+        self.capacidade_atual = 0
+
+    def encher_bomba(self):
+        self.capacidade_atual = self.capacidade_maxima
+
+    def consultar_reserva(self):
+        return self.capacidade_atual
+
+    def abastecer_valor(self, dinheiro_recebido):
+        if (dinheiro_recebido/self.preco_litro) <= self.capacidade_atual:
+            self.capacidade_atual-= dinheiro_recebido/self.preco_litro
+            return dinheiro_recebido/self.preco_litro
+        else:
+            return 'Reserva Insuficiente. Operação Abortada'
+
+    def abastecer_litro(self, litros_desejados):
+        if self.capacidade_atual >= litros_desejados:
+            self.capacidade_atual-=litros_desejados
+            return self.preco_litro*litros_desejados
+        else:
+            return 'Reserva Insuficiente. Operação Abortada'
---
+++ 7/testbombacombustivel.py
@@ -0,0 +1,51 @@
+#coding: utf-8
+import unittest
+from should_dsl import should
+from bombacombustivel import Bomba
+
+# 7) Crie uma classe que modele uma bomba de combustível
+class TestBomba(unittest.TestCase):
+
+    #e permita definir a capacidade da bomba, o preço do combustível por litro
+    def test_bomba(self):
+        bomba = Bomba(5000, 1.689)
+        bomba.capacidade_maxima |should| equal_to(5000)
+        bomba.preco_litro |should| equal_to(1.689)
+        bomba.capacidade_atual |should| equal_to(0)
+
+    #permita encher a bomba (sempre encher completamente) e consultar sua quantidade de combustível.
+    def test_encher_bomba(self):
+        bomba = Bomba(5000, 1.689)
+        bomba.encher_bomba()
+        bomba.capacidade_atual |should| equal_to(5000)
+
+    def test_consultar_reserva(self):
+        bomba = Bomba(5000, 1.689)
+        bomba.consultar_reserva() |should| equal_to(0)
+        bomba.encher_bomba()
+        bomba.consultar_reserva() |should| equal_to(5000)
+
+    #seja permitido abastecer por valor (retornando a quantidade de combustível abastecida)
+    def test_abaster_por_preco(self):
+        bomba = Bomba(5000, 1.689)
+        bomba.encher_bomba()
+        bomba.abastecer_valor(16.89) |should| equal_to(10)
+
+    #abastecer por quantidade de litros (retornando o valor a ser pago).
+    def test_abaster_por_litro(self):
+        bomba = Bomba(5000, 1.689)
+        bomba.encher_bomba()
+        bomba.abastecer_litro(10) |should| equal_to(16.89)
+
+    #Deve ser tratada a situação de não haver combustível suficiente na bomba para um determinado abastecimento.
+    def test_abastecer_litro_alem_reserva(self):
+        bomba = Bomba(5000, 1.689)
+        bomba.capacidade_atual |should| equal_to(0)
+        bomba.abastecer_litro(10) |should| equal_to('Reserva Insuficiente. Operação Abortada')
+
+    def test_abastecer_preco_alem_reserva(self):
+        bomba = Bomba(5000, 1.689)
+        bomba.abastecer_valor(16.89) |should| equal_to('Reserva Insuficiente. Operação Abortada')
+
+if __name__ == "__main__":
+    unittest.main()
